9634097619c7691e1dd5dd6cdc6d437568010658
Xinyi Chen <shandy@novanb.com>
2022-07-01 02:09:41 +1000

    7,8

--- flask_app.py
+++ flask_app.py
@@ -7,15 +7,14 @@ import os
 app = Flask(__name__)
 hops = hs.Hops(app)
 
-url = 'https://data.townofcary.org/api/records/1.0/search/?dataset=building-points&q=&rows=20&sort=-rooms&facet=building_type&facet=building_sub_type&facet=bldgstyle&facet=yearbuilt&facet=storyheight&facet=basement&facet=utilities&refine.building_sub_type=2+Family&refine.building_sub_type=3+Family&refine.building_sub_type=4+Family&refine.building_sub_type=Multi-Family&refine.building_sub_type=Single+Family&refine.building_type=Residential'
-r = requests.get(url)
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, I am Shandy from UNSW CODE2120 </p>"
 
 
-@hops.component_addMatrix(
+@hops.component_Matrix(
     "/np_addMatrix",
     name= "np_addMatrix",
     description="np_addMatrix",
@@ -58,6 +57,8 @@ def np_addMatrix(M1,M2):
     return list(result)
 
 def buildinginfor_basement(PercentBasement):
+    url = 'https://data.townofcary.org/api/records/1.0/search/?dataset=building-points&q=&rows=20&sort=-rooms&facet=building_type&facet=building_sub_type&facet=bldgstyle&facet=yearbuilt&facet=storyheight&facet=basement&facet=utilities&refine.building_sub_type=2+Family&refine.building_sub_type=3+Family&refine.building_sub_type=4+Family&refine.building_sub_type=Multi-Family&refine.building_sub_type=Single+Family&refine.building_type=Residential'
+    r = requests.get(url)
     for option in r.json()[PercentBasement]:
         v = r(PercentBasement)
     print(v)
