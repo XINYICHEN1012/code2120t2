e90027bef48340c45415a01eb2a008ab18ac5421
Xinyi Chen <shandy@novanb.com>
2022-07-01 02:36:07 +1000

    1,2

--- flask_app.py
+++ flask_app.py
@@ -14,7 +14,7 @@ def hello_world():
     return "<p>Hello, I am Shandy from UNSW CODE2120 </p>"
 
 
-@hops.component(
+##@hops.component(
     "/np_addMatrix",
     name= "np_addMatrix",
     description="np_addMatrix",
@@ -27,19 +27,18 @@ def hello_world():
     ]
 )
 
-@app.route("/urlend")
-def np_addMatrix(M1,M2):
-    import numpy as np
-    import json
-    matrix1 = np.array(M1).reshape((2,2))
-    matrix2 = np.array(M2).reshape((2,2))
+##@app.route("/urlend")
+##def np_addMatrix(M1,M2):
+   ## import numpy as np
+   # import json
+   # matrix1 = np.array(M1).reshape((3,1))
+   # matrix2 = np.array(M2).reshape((3,1))
+   # result = np.add(matrix1,matrix2)
+   #  result = result.flatten('F')
+   #  print(result)
+   # return list(result)
 
-    result = np.add(matrix1,matrix2)
-    result = result.flatten('F')
-    print(result)
-    return list(result)
-
-##@hops.component_2(
+@hops.component(
     "/clear_dictionary",
     name= "clear_dictionary",
     description="clear_dictionary",
@@ -50,15 +49,15 @@ def np_addMatrix(M1,M2):
     outputs=[
         hs.HopsString("information")
     ]
-##)
+)
 
-##@app.route("/urlend")
-##def buildinginfor_basement(PercentBasement):
-##    url = 'https://data.townofcary.org/api/records/1.0/search/?dataset=building-points&q=&rows=20&sort=-rooms&facet=building_type&facet=building_sub_type&facet=bldgstyle&facet=yearbuilt&facet=storyheight&facet=basement&facet=utilities&refine.building_sub_type=2+Family&refine.building_sub_type=3+Family&refine.building_sub_type=4+Family&refine.building_sub_type=Multi-Family&refine.building_sub_type=Single+Family&refine.building_type=Residential'
-##    r = requests.get(url)
-##    for option in r.json()[PercentBasement]:
-##        v = r(PercentBasement)
-##    print(v)
+@app.route("/urlend")
+def buildinginfor_basement(PercentBasement):
+    url = 'https://data.townofcary.org/api/records/1.0/search/?dataset=building-points&q=&rows=20&sort=-rooms&facet=building_type&facet=building_sub_type&facet=bldgstyle&facet=yearbuilt&facet=storyheight&facet=basement&facet=utilities&refine.building_sub_type=2+Family&refine.building_sub_type=3+Family&refine.building_sub_type=4+Family&refine.building_sub_type=Multi-Family&refine.building_sub_type=Single+Family&refine.building_type=Residential'
+    r = requests.get(url)
+    for option in r.json()[PercentBasement]:
+        v = r(PercentBasement)
+    print(v)
 
 if __name__ == '__main__':
     app.run()
