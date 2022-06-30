2c70b65c579f48e1af623b94ec2535efb0e46665
Xinyi Chen <shandy@novanb.com>
2022-06-30 17:44:21 +1000

    3

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import ghhops_server as hs
+from matplotlib.font_manager import json_dump
 
 
 app = Flask(__name__)
@@ -14,8 +15,8 @@ def hello_world():
     name= "np_addMatrix",
     description="np_addMatrix",
     inputs=[
-        hs.HopsNumber("M1","M1","M1"),
-        hs.HopsNumber("M2","M2","M2"),
+        hs.HopsNumber("M1","M1","M1",access=hs.HopsParamAccess.LIST),
+        hs.HopsNumber("M2","M2","M2",access=hs.HopsParamAccess.LIST),
     ],
     outputs=[
         hs.HopsNumber("M3","M3","M3")
@@ -26,12 +27,14 @@ def hello_world():
 @app.route("/urlend")
 def np_addmatrix(M1,M2):
     import numpy as np
-
+    import json
     matrix1 = np.array(M1).reshape((2,2))
     matrix2 = np.array(M2).reshape((2,2))
 
     result = np.add(matrix1,matrix2)
-    return result
+    result = result.flatten('F')
+    print(result) 
+    return json.dumps(list(result))
 
 
 if __name__ == '__main__':
